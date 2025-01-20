7b8ad3712bc1659c40755ca7691e8da3d9afdde0
VitorOdorico <ve743137@gmail.com>
2025-01-20 07:43:31 -0300

    start project

---
+++ src/test/java/com/main/montadora/MontadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.montadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
